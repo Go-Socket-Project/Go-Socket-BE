2976e62a93068a8e3540b3d3ac284e12a8c8fd11
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-06-05 13:57:59 +0900

    :sparkles: dj-rest-auth 회원가입 & 로그인 추가

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -3,7 +3,5 @@ from django.urls import path,include
 urlpatterns = [
     # 일반 회원 회원가입/로그인
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
-
-    # 구글 소셜 로그인
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
