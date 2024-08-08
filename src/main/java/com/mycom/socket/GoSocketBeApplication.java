f8b896a8a8b4e02b28e5f0e848a5a4b7602771af
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-08 16:38:23 +0900

    :tada: initial commit

---
+++ src/main/java/com/mycom/socket/GoSocketBeApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoSocketBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoSocketBeApplication.class, args);
+    }
+
+}
