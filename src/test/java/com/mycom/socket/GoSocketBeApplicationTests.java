f0e291f4e7d7dd5e76cb98313f40c8844859a988
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-08 16:38:23 +0900

    :sparkles: init commit

---
+++ src/test/java/com/mycom/socket/GoSocketBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoSocketBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
