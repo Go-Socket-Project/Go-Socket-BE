8a35e5ba400d651948bff7b5e83e23e1b91b61f9
오유찬 <oyuchan50@gmail.com>
2025-01-07 21:55:47 +0900

    :sparkles: Email 인증 번호 전송 및 체크 기능 구현

--- src/main/java/com/mycom/socket/auth/controller/AuthController.java
+++ src/main/java/com/mycom/socket/auth/controller/AuthController.java
@@ -4,13 +4,11 @@ import com.mycom.socket.auth.dto.request.LoginRequestDto;
 import com.mycom.socket.auth.dto.request.RegisterRequestDto;
 import com.mycom.socket.auth.dto.response.LoginResponseDto;
 import com.mycom.socket.auth.service.AuthService;
+import com.mycom.socket.auth.service.MailService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -18,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final MailService mailService;
+    private int number;
 
     @PostMapping("/login")
     public LoginResponseDto login(@Valid @RequestBody LoginRequestDto request,
@@ -34,4 +34,20 @@ public class AuthController {
     public Long register(@Valid @RequestBody RegisterRequestDto request) {
         return authService.register(request);
     }
+
+    @PostMapping("/mail-send")
+    public Integer mailSend(@RequestParam(name = "mail") String mail) {
+        try {
+            number = mailService.sendMail(mail);
+            return number;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send mail: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/mail-check")
+    public Boolean mailCheck(@RequestParam(name = "userNumber") String userNumber) {
+        return userNumber.equals(String.valueOf(number));
+    }
+
 }
