1da4aefe1712316bba5bd12f07f87421efc2b2ba
오유찬 <oyuchan50@gmail.com>
2025-01-09 00:22:46 +0900

    :recycle: ApiResponse 전송 방식 양식 변경

--- src/main/java/com/mycom/socket/global/dto/ApiResponse.java
+++ src/main/java/com/mycom/socket/global/dto/ApiResponse.java
@@ -2,29 +2,53 @@ package com.mycom.socket.global.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T>(
+        boolean success,
         String message,
         T data,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         LocalDateTime timestamp
 ) {
+        @Builder
+        public  ApiResponse {
+        }
         public static <T> ApiResponse<T> success(String message) {
-                return new ApiResponse<>(message, null, LocalDateTime.now());
+                return ApiResponse.<T>builder()
+                        .success(true)
+                        .message(message)
+                        .timestamp(LocalDateTime.now())
+                        .build();
         }
 
         public static <T> ApiResponse<T> success(String message, T data) {
-                return new ApiResponse<>(message, data, LocalDateTime.now());
+                return ApiResponse.<T>builder()
+                        .success(true)
+                        .message(message)
+                        .data(data)
+                        .timestamp(LocalDateTime.now())
+                        .build();
         }
 
         public static <T> ApiResponse<T> error(String message) {
-                return new ApiResponse<>(message, null, LocalDateTime.now());
+                return ApiResponse.<T>builder()
+                        .success(false)
+                        .message(message)
+                        .timestamp(LocalDateTime.now())
+                        .build();
         }
 
         public static <T> ApiResponse<T> error(String message, T data) {
-                return new ApiResponse<>(message, data, LocalDateTime.now());
+                return ApiResponse.<T>builder()
+                        .success(false)
+                        .message(message)
+                        .data(data)
+                        .timestamp(LocalDateTime.now())
+                        .build();
         }
+
 }
