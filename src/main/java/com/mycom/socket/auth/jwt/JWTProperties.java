d6c1d3f7810fb74b0d3610906562897360ddcd35
오유찬 <oyuchan50@gmail.com>
2025-01-09 02:32:39 +0900

    :recycle: 쿠키와 yml 설정 분리 및 코드 리팩토링

---
+++ src/main/java/com/mycom/socket/auth/jwt/JWTProperties.java
@@ -0,0 +1,15 @@
+package com.mycom.socket.auth.jwt;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JWTProperties {
+    private String secret;
+    private long accessTokenValidityInSeconds = 1800;
+    private String cookieName = "Authorization";
+    private String issuer = "go_socket";
+    private boolean secureCookie = false;
+}
