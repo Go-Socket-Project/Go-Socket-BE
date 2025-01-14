fa72e44b5e7ae441ac683d8a65925f0bcabd9fd5
오유찬 <oyuchan50@gmail.com>
2025-01-14 11:52:17 +0900

    :recycle: MailService 코드 리팩토링

---
+++ src/main/java/com/mycom/socket/auth/config/MailProperties.java
@@ -0,0 +1,21 @@
+package com.mycom.socket.auth.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "mail")
+public class MailProperties {
+    private String host;
+    private int port;
+    private String protocol;
+    private String username;
+    private String password;
+    private String senderEmail;
+    private String subject;
+    private String bodyTemplate;
+}
