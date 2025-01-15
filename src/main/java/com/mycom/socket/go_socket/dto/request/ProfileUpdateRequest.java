51c428470de8a3fd92c0851be74fc5297ad98bfe
오유찬 <oyuchan50@gmail.com>
2025-01-15 18:49:34 +0900

    :sparkles: request 상세화

--- src/main/java/com/mycom/socket/go_socket/dto/request/ProfileUpdateRequest.java
+++ src/main/java/com/mycom/socket/go_socket/dto/request/ProfileUpdateRequest.java
@@ -1,6 +1,12 @@
 package com.mycom.socket.go_socket.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record ProfileUpdateRequest(
+        @NotBlank(message = "닉네임은 필수입니다")
+        @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하여야 합니다")
         String nickname,
+        @Size(max = 100, message = "자기소개는 100자를 초과할 수 없습니다")
         String intro
 ) {}
